2c554d2c2a74f6a182c8931b2071fca9497a4015
gnawsti <awang0124@gmail.com>
2016-02-11 02:07:51 -0500

    Basic html working with placeholder links and updated title.

--- p1.php
+++ p1.php
@@ -2,8 +2,23 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Andrew Wang</title>
+	<title>Andrew Wang P1- DWA15</title>
 </head>
 <body>
+	<h1>Andrew Wang</h1>
+	<img src=https://scontent-lga3-1.xx.fbcdn.net/hphotos-ash2/v/t1.0-9/487337_10152295886375613_1920159794_n.jpg?oh=b6c03615c11f85b1e6a9fdc49149b948&oe=5722E1D4 alt="Profile Picture">
+	<div id="projects">
+		<h3>Project 2</h3>
+		<ul>
+			<li>See it <a href="">here</a>!</li>
+			<li><a href="">Github</a></li>
+		</ul>
+
+		<h3>Project 3</h3>
+		<ul>
+			<li>See it <a href="">here</a>!</li>
+			<li><a href="">Github</a></li>
+		</ul>
+	</div>
 </body>
 </html>
