70b66723ce4787cbc73efdd81d902eea74c895f9
gnawsti <awang0124@gmail.com>
2016-02-11 02:34:14 -0500

    Added css file to center everything. Removed some bootstrap classes that seemed to be conflicting with the centering.

--- p1.php
+++ p1.php
@@ -5,22 +5,19 @@
 	<title>Andrew Wang P1- DWA15</title>
 	<!-- Bootstrap -->
 	<link rel="stylesheet" href="bootstrap.min.css"/>
+	<!-- Custom CSS -->
+	<link rel="stylesheet" href="p1.css"
 </head>
 <body>
 	<h1>Andrew Wang</h1>
 	<img src=https://scontent-lga3-1.xx.fbcdn.net/hphotos-ash2/v/t1.0-9/487337_10152295886375613_1920159794_n.jpg?oh=b6c03615c11f85b1e6a9fdc49149b948&oe=5722E1D4 alt="Profile Picture">
 	<div id="projects">
 		<h3>Project 2</h3>
-		<ul>
-			<li>See it <a href="">here</a>!</li>
-			<li><a href="">Github</a></li>
-		</ul>
-
+		<p>See it <a href="">here</a>!</p>
+		<p><a href="">Github</a></p>
 		<h3>Project 3</h3>
-		<ul>
-			<li>See it <a href="">here</a>!</li>
-			<li><a href="">Github</a></li>
-		</ul>
+		<p>See it <a href="">here</a>!</p>
+		<p><a href="">Github</a></p>
 	</div>
 </body>
 </html>
